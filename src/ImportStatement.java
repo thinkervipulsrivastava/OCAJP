1ccd830f43485a17829e8400de1f447ca4451d60
Vipul Srivastava <thinkervipulsrivastava@gmail.com>
2020-08-31 19:26:05 +0530

    import

---
+++ src/ImportStatement.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class ImportStatement {
+    public static void main (String [] args) {
+        //java.util.List<Integer> list = new java.util.ArrayList<Integer>();
+        List<Integer> list = new ArrayList<Integer>();
+    }
+}
--- src/Scope.java
+++ src/Scope.java
@@ -9,9 +9,10 @@ public class Scope {
     static int number1 = 0;
 
     public static void main(String[] args) {
+
         {
             // Block level Scope
-            int number2 = 0;
+             number1 = 2;
         }
         System.out.println(number1);
         int number3 = 0;
